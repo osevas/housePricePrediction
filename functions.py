5319972c5f93eb10681487ce4cf3fef0ca1d4510
Onur Sevket Aslan <50531008+osevas@users.noreply.github.com>
2020-12-14 00:15:18 +0300

    Code is complete. Predictions have been created.  Result is to be uploaded.

--- functions.py
+++ functions.py
@@ -1,6 +1,9 @@
 import numpy as np
+import pandas as pd
 from sklearn.metrics import mean_squared_log_error
 from sklearn import preprocessing
+from sklearn.feature_selection import f_regression
+import matplotlib.pyplot as plt
 
 def rmsle(y_true,y_pred):
     '''
@@ -45,3 +48,36 @@ def remove_outlier(df,Serie):
     df=df[df[Serie]<=(df[Serie].quantile(q=0.75)+1.5*IQR)]
     df.reset_index(inplace=True, drop=True)
     return df
+
+def f_Regression(df,y,columnsInput):
+    '''
+    Selects columns with good correlation with target columns
+    Input:
+    df: DataFrame
+    y: target column
+    columnsInput: list of columns to evaluate
+
+    Output:
+    list of columns to select
+    '''
+    f_test,p_val=f_regression(df[columnsInput],y)
+    scores=f_test/f_test.max()
+
+    # DISCUSSION: set f-score limit
+    f_score_lim=0.1
+
+    plt.figure()
+    plt.bar(x=np.arange(p_val.shape[0]),height=scores,label='F-scores normalized')
+    plt.xticks(ticks=np.arange(p_val.shape[0]),labels=columnsInput,rotation='vertical')
+    plt.axhline(y=f_score_lim)
+    plt.ylabel('F-test scores normalized')
+    plt.show()
+
+    df_1_f_reg=pd.DataFrame(data={'columns':columnsInput,'F-scores normalized':scores})
+    # plt.figure()
+    # plt.hist(df_1_f_reg['F-scores normalized'])
+    # plt.show()
+
+    columnsToSelect=df_1_f_reg[df_1_f_reg['F-scores normalized']>=f_score_lim]['columns'].to_list() # Column names that could be used in prediction
+    return columnsToSelect
+    
--- main_housePrices.py
+++ main_housePrices.py
@@ -17,13 +17,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.feature_selection import VarianceThreshold, f_regression
-from sklearn.impute import KNNImputer
-from sklearn.model_selection import train_test_split
+from sklearn.impute import KNNImputer,SimpleImputer
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn import preprocessing
 from sklearn import linear_model, tree
 from sklearn.metrics import mean_squared_log_error
 import functions
 from sklearn.ensemble import AdaBoostRegressor
+from importlib import reload
 
 
 
@@ -40,27 +41,14 @@ df=pd.read_csv('train.csv')
 # --------------------------------------------
 
 # Assessing Y 
-fig,axs=plt.subplots(nrows=1,ncols=2)
-axs[0].hist(df['SalePrice'])
-axs[0].set_title('SalePrice with Outliers')
+# fig,axs=plt.subplots(nrows=1,ncols=2)
+# axs[0].hist(df['SalePrice'])
+# axs[0].set_title('SalePrice with Outliers')
 
-axs[1].boxplot(df['SalePrice'])
-axs[1].set_title('SalePrice with Outliers')
+# axs[1].boxplot(df['SalePrice'])
+# axs[1].set_title('SalePrice with Outliers')
 
-plt.show()
-
-# Need to remove outliers in Y
-
-df=functions.remove_outlier(df,'SalePrice')
-
-fig,axs=plt.subplots(nrows=1,ncols=2)
-axs[0].hist(df['SalePrice'])
-axs[0].set_title('SalePrice with no Outliers')
-
-axs[1].boxplot(df['SalePrice'])
-axs[1].set_title('SalePrice with no Outliers')
-
-plt.show()
+# plt.show()
 
 
 # %%--------------------------------------------
@@ -158,17 +146,17 @@ continuous_cols=continuous_cols[:-1] #removing 'SalePrice' from continuous colum
 year_cols=column_df[column_df['col_category']==3]['cols'].to_list()
 # Column 'GarageYrBlt' has null values
 
-fig, axs=plt.subplots(nrows=1,ncols=4,figsize=(20,20))
-for i,col in enumerate(year_cols):
-    axs[i].hist(df_1[col])
-    axs[i].set_title(col)
-plt.show()
+# fig, axs=plt.subplots(nrows=1,ncols=4,figsize=(20,20))
+# for i,col in enumerate(year_cols):
+#     axs[i].hist(df_1[col])
+#     axs[i].set_title(col)
+# plt.show()
 
-fig, axs=plt.subplots(nrows=1,ncols=4,figsize=(20,20))
-for i,col in enumerate(year_cols):
-    axs[i].scatter(df_1[col],df_1['SalePrice'])
-    axs[i].set_title(col)
-plt.show()
+# fig, axs=plt.subplots(nrows=1,ncols=4,figsize=(20,20))
+# for i,col in enumerate(year_cols):
+#     axs[i].scatter(df_1[col],df_1['SalePrice'])
+#     axs[i].set_title(col)
+# plt.show()
 
 #%% --------------------------------------------
 # Feature Analysis for columns with type=4
@@ -176,17 +164,17 @@ plt.show()
 count_cols=column_df[column_df['col_category']==4]['cols'].to_list()
 # count_cols have no null values
 
-fig, axs=plt.subplots(nrows=1,ncols=10,figsize=(30,10))
-for i,col in enumerate(count_cols):
-    axs[i].hist(df_1[col])
-    axs[i].set_title(col)
-plt.show()
+# fig, axs=plt.subplots(nrows=1,ncols=10,figsize=(30,10))
+# for i,col in enumerate(count_cols):
+#     axs[i].hist(df_1[col])
+#     axs[i].set_title(col)
+# plt.show()
 
-fig, axs=plt.subplots(nrows=1,ncols=10,figsize=(30,10))
-for i,col in enumerate(count_cols):
-    axs[i].scatter(df_1[col],df_1['SalePrice'])
-    axs[i].set_title(col)
-plt.show()
+# fig, axs=plt.subplots(nrows=1,ncols=10,figsize=(30,10))
+# for i,col in enumerate(count_cols):
+#     axs[i].scatter(df_1[col],df_1['SalePrice'])
+#     axs[i].set_title(col)
+# plt.show()
 
 #%% --------------------------------------------
 # Feature Analysis for columns with type=5
@@ -194,44 +182,20 @@ plt.show()
 price_cols=column_df[column_df['col_category']==5]['cols'].to_list()
 # count_cols have no null values
 
-plt.figure(figsize=(30,10))
-plt.hist(df_1[price_cols[0]])
-plt.title(price_cols[0])
-plt.show()
+# plt.figure(figsize=(30,10))
+# plt.hist(df_1[price_cols[0]])
+# plt.title(price_cols[0])
+# plt.show()
 
-plt.figure(figsize=(30,10))
-plt.scatter(df_1[price_cols[0]],df_1['SalePrice'])
-plt.title(price_cols[0])
-plt.show()
-
-#%% --------------------------------------------
-# Feature Analysis for columns with type=1 (categorical)
-# ----------------------------------------------
-categorical_cols=column_df[column_df['col_category']==1]['cols'].to_list()
-
-# Dropping categorical features with missing values.  Deciding not to impute
-df_1.drop(columns=['Alley','MasVnrType','BsmtQual','BsmtCond',
-    'BsmtExposure','BsmtFinType1','BsmtFinType2','Electrical',
-    'FireplaceQu','GarageType','GarageFinish','GarageQual',
-    'GarageCond','PavedDrive','PoolQC','Fence','MiscFeature'],inplace=True)
-
-#%% --------------------------------------------
-# Feature Selection for columns with type=1 (categorical)
-# ----------------------------------------------
-
-
-
-
-
-
-
-
-
+# plt.figure(figsize=(30,10))
+# plt.scatter(df_1[price_cols[0]],df_1['SalePrice'])
+# plt.title(price_cols[0])
+# plt.show()
 
 
 
 #%% --------------------------------------------
-# Imputation for missing values
+# Imputation for missing values for columns in types=2-5
 # --------------------------------------------
 # using KNNImputer to fill in nulls in 
 
@@ -253,27 +217,8 @@ if sel_variance_result.shape[1]==df_1[continuous_cols].shape[1]:
     print('Variance Threshold is not working\n')
 
 # Trying f_regression for feature selection
-f_test,p_val=f_regression(df_1[continuous_cols],df_1['SalePrice'])
-# print(f_test/f_test.max())
-
-scores=f_test/f_test.max()
-
-# DISCUSSION: set f-score limit
-f_score_lim=0.1
-
-plt.figure()
-plt.bar(x=np.arange(p_val.shape[0]),height=scores,label='F-scores normalized')
-plt.xticks(ticks=np.arange(p_val.shape[0]),labels=continuous_cols,rotation='vertical')
-plt.axhline(y=f_score_lim)
-plt.ylabel('F-test scores normalized')
-plt.show()
-
-df_1_f_reg=pd.DataFrame(data={'columns':continuous_cols,'F-scores normalized':scores})
-# plt.figure()
-# plt.hist(df_1_f_reg['F-scores normalized'])
-# plt.show()
-
-continousCols=df_1_f_reg[df_1_f_reg['F-scores normalized']>=f_score_lim]['columns'].to_list() # Column names that could be used in prediction
+print('Performing f-regression for continuous columns:')
+continousCols=functions.f_Regression(df_1,df_1['SalePrice'],continuous_cols)
 continousCols.append('SalePrice')
 # sns.pairplot(df_1[continousCols])
 # plt.show()
@@ -284,6 +229,51 @@ continousCols.append('SalePrice')
 
 del continousCols[-1]
 
+
+#%% --------------------------------------------
+# Feature Analysis for columns with type=1 (categorical)
+# ----------------------------------------------
+categorical_cols=column_df[column_df['col_category']==1]['cols'].to_list()
+
+# Dropping categorical features with missing values.  Deciding not to impute
+columns_toDrop=['Alley','MasVnrType','BsmtQual','BsmtCond',
+    'BsmtExposure','BsmtFinType1','BsmtFinType2','Electrical',
+    'FireplaceQu','GarageType','GarageFinish','GarageQual',
+    'GarageCond','PavedDrive','PoolQC','Fence','MiscFeature']
+for col in columns_toDrop:
+    categorical_cols.remove(col)
+
+#%% --------------------------------------------
+# Feature Selection for columns with type=1 (categorical)
+# ----------------------------------------------
+# Encoding categorical columns
+enc=preprocessing.OrdinalEncoder()
+enc.fit(df_1[categorical_cols])
+df_1[categorical_cols]=enc.transform(df_1[categorical_cols])
+
+# enc2=preprocessing.OneHotEncoder()
+# enc2.fit(df_1[categorical_cols])
+# categoricalCols_transformed=enc2.transform(df_1[categorical_cols]).toarray()
+
+# Selecting columns that will be used in the models
+print('Performing f-regression for categorical columns:')
+categoricalCols=functions.f_Regression(df_1,df_1['SalePrice'],categorical_cols)
+
+
+
+# fig, axs=plt.subplots(nrows=1,ncols=len(categoricalCols),figsize=(30,10))
+# for i,col in enumerate(categoricalCols):
+#     axs[i].hist(df_1[col])
+#     axs[i].set_title(col)
+# plt.show()
+
+# fig, axs=plt.subplots(nrows=1,ncols=len(categoricalCols),figsize=(30,10))
+# for i,col in enumerate(categoricalCols):
+#     axs[i].scatter(df_1[col],df_1['SalePrice'])
+#     axs[i].set_title(col)
+# plt.show()
+
+
 #%% --------------------------------------------
 # Outlier removal from features
 # ----------------------------------------------
@@ -292,14 +282,29 @@ del continousCols[-1]
 for column in continousCols[1:5]:
     df_1=functions.remove_outlier(df_1,column)
 
+# Need to remove outliers in Y
+df=functions.remove_outlier(df,'SalePrice')
+
+# fig,axs=plt.subplots(nrows=1,ncols=2)
+# axs[0].hist(df['SalePrice'])
+# axs[0].set_title('SalePrice with no Outliers')
+
+# axs[1].boxplot(df['SalePrice'])
+# axs[1].set_title('SalePrice with no Outliers')
+
+# plt.show()
+
+
 #%% --------------------------------------------
 # Train-valid-split & Scaling
 # --------------------------------------------
 y=df_1['SalePrice']
-X=df_1[continousCols]
-
+# X=df_1[continousCols]
+X=df_1[continousCols].merge(df_1[categoricalCols],left_index=True,right_index=True)
+# X=df_1[continousCols].to_numpy()
+# X=np.concatenate((X,categoricalCols_transformed),axis=1)
 # train_valid_split
-X_train,X_valid,y_train,y_valid=train_test_split(X,y,test_size=0.40)
+X_train,X_valid,y_train,y_valid=train_test_split(X,y,test_size=0.40,random_state=42)
 
 # Tried mapping non-Gaussian features to Gaussian, but it did not give good result
 #X_train[['MasVnrArea','BsmtFinSF1']]=functions.map_to_Gaussian(X_train[['MasVnrArea','BsmtFinSF1']],'yeo-johnson')
@@ -308,9 +313,8 @@ X_train,X_valid,y_train,y_valid=train_test_split(X,y,test_size=0.40)
 # Scaling
 # scaler=preprocessing.StandardScaler() # StandardScaler produces negative values, which become problematic later in calculating RMLSE
 # X_train_scaled=scaler.fit_transform(X_train)
-
-X_train_scaled=preprocessing.minmax_scale(X_train,feature_range=(0,1),axis=0,copy=True)
-
+X_train_scaled=preprocessing.minmax_scale(X_train[continousCols],feature_range=(0,1),axis=0,copy=True)
+X_train_scaled=np.concatenate((X_train_scaled,X_train[categoricalCols].to_numpy()),axis=1)
 # X_train_scaled=pd.DataFrame(data=X_train_scaled,columns=continousCols)
 # # Evaluating scaling of train
 # fig,ax=plt.subplots(nrows=5,ncols=2,figsize=(10,20))
@@ -327,7 +331,8 @@ X_train_scaled=preprocessing.minmax_scale(X_train,feature_range=(0,1),axis=0,cop
 # Scaling validation set
 # X_valid_scaled=scaler.transform(X_valid) # StandardScaler produces negative values, which become problematic later in calculating RMLSE
 
-X_valid_scaled=preprocessing.minmax_scale(X_valid,feature_range=(0,1),axis=0,copy=True)
+X_valid_scaled=preprocessing.minmax_scale(X_valid[continousCols],feature_range=(0,1),axis=0,copy=True)
+X_valid_scaled=np.concatenate((X_valid_scaled,X_valid[categoricalCols].to_numpy()),axis=1)
 
 # X_valid_scaled=pd.DataFrame(data=X_valid_scaled,columns=continousCols)
 # # Evaluating scaling of validation
@@ -363,14 +368,14 @@ print('Test R^2 of ordinary least squares: {:.3}'.format(test_r2_linreg)) #calcu
 
 #Linear Regression predicts a negative value.  This causes RMSLE not to be calculated.
 
-# y_train_predict=reg.predict(X_train_scaled)
-# y_valid_predict=reg.predict(X_valid_scaled)
+y_train_predict=reg.predict(X_train_scaled)
+y_valid_predict=reg.predict(X_valid_scaled)
 
-# train_rmsle_linreg=functions.rmsle(y_train,y_train_predict)
-# test_rmsle_linreg=functions.rmsle(y_valid,y_valid_predict)
+train_rmsle_linreg=functions.rmsle(y_train,y_train_predict)
+test_rmsle_linreg=functions.rmsle(y_valid,y_valid_predict)
 
-# print('Train RMSLE of ordinary least squares: {:.3}'.format(train_rmsle_linreg)) #calculating train rmsle
-# print('Test RMSLE of ordinary least squares: {:.3}'.format(test_rmsle_linreg)) #calculating valid rmsle
+print('Train RMSLE of ordinary least squares: {:.3}'.format(train_rmsle_linreg)) #calculating train rmsle
+print('Test RMSLE of ordinary least squares: {:.3}'.format(test_rmsle_linreg)) #calculating valid rmsle
 
 #%% --------------------------------------------
 # Fit & Prediction of Decision Tree Regressor
@@ -396,7 +401,7 @@ print('Test RMSLE of DecisionTreeRegressor: {:.3}'.format(test_rmsle_DTreg)) #ca
 #%% --------------------------------------------
 # Fit & Prediction of AdaBoost Regressor
 # --------------------------------------------
-regr=AdaBoostRegressor(loss='exponential')
+regr=AdaBoostRegressor(loss='linear',n_estimators=50,learning_rate=1,random_state=31)
 regr.fit(X_train_scaled,y_train)
 
 y_train_predict=regr.predict(X_train_scaled)
@@ -414,6 +419,21 @@ print('Test R^2 of AdaBoost Regressor: {:.3}'.format(test_r2_ABreg)) #calculatin
 print('Train RMSLE of AdaBoost Regressor: {:.3}'.format(train_rmsle_ABreg)) #calculating train rmsle
 print('Test RMSLE of AdaBoost Regressor: {:.3}'.format(test_rmsle_ABreg)) #calculating valid rmsle
 
+#%% --------------------------------------------
+# GridSearch for AdaBoost Regressor
+# --------------------------------------------
+
+# param_grid=[
+#     {'n_estimators':[50,70,90],'learning_rate':[0.01,0.1,1],
+#     'loss':['linear','square','exponential']}
+#     ]
+
+# clf=GridSearchCV(AdaBoostRegressor(),param_grid,scoring='r2',verbose=5)
+# clf.fit(X_train_scaled,y_train)
+
+# clf.best_estimator_
+# clf.best_score_
+# clf.best_params_
 
 #%% --------------------------------------------
 # Comparison of y_train and y_train_predict
@@ -438,8 +458,8 @@ plt.show()
 model_result=pd.DataFrame(data={'Model':['Linear Regression','Decision Tree Regressor','AdaBoost Regressor'],
     'Train R2':[train_r2_linreg,train_r2_DTreg,train_r2_ABreg],
     'Test R2':[test_r2_linreg,test_r2_DTreg,test_r2_ABreg],
-    'Train RMSLE':['N/A',train_rmsle_DTreg,train_rmsle_ABreg],
-    'Test RMSLE':['N/A',test_rmsle_DTreg,test_rmsle_ABreg]})
+    'Train RMSLE':[train_rmsle_linreg,train_rmsle_DTreg,train_rmsle_ABreg],
+    'Test RMSLE':[test_rmsle_linreg,test_rmsle_DTreg,test_rmsle_ABreg]})
 print(model_result)
 #%% --------------------------------------------
 # Residual plot
@@ -452,4 +472,31 @@ axs[1].hist(y_valid-reg.predict(X_valid_scaled))
 axs[1].set_title('Residual plot of test set')
 plt.show()
 
+#************RUN THE MODEL ON THE TEST DATA**************************
+
+#%% --------------------------------------------
+# Test data ingestion
+# --------------------------------------------
+df_test=pd.read_csv('test.csv')
+
+#%% --------------------------------------------
+# Scaling and encoding
+# --------------------------------------------
+df_test[continuous_cols]=imputer.transform(df_test[continuous_cols])
+X_test_scaled=preprocessing.minmax_scale(df_test[continousCols],feature_range=(0,1),axis=0,copy=True)
+
+# Performing simple imputer for 
+imp_mode=SimpleImputer(strategy='most_frequent')
+imp_mode.fit(df_test[categorical_cols])
+df_test[categorical_cols]=imp_mode.transform(df_test[categorical_cols])
+
+# Encoding categorical columns
+df_test[categorical_cols]=enc.transform(df_test[categorical_cols])
+X_test_scaled=np.concatenate((X_test_scaled,df_test[categoricalCols].to_numpy()),axis=1)
+#%% --------------------------------------------
+# Prediction
+# --------------------------------------------
+y_test_predict=regr.predict(X_test_scaled)
+# %%
+prediction=pd.DataFrame(data={'Id':df_test['Id'].to_numpy(),'SalePrice':y_test_predict})
 # %%
